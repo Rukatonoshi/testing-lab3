035107215d5958c1d479a863a4ff2de973ac7282
Rukatonoshi <Rukatonoshi@users.noreply.github.com>
2025-03-25 09:47:18 +0000

    deploy: 6f563808ebd518318fcd85ff3bf82415b315e7b5

--- src/test/java/AppRunner.java
+++
@@ -1,7 +0,0 @@
-import org.itmo.testing.lab2.controller.UserAnalyticsController;
-
-public class AppRunner {
-    public static void main(String[] args) {
-        UserAnalyticsController.createApp().start(7000);
-    }
-}
